c5c005ce1d4280abad85ea4c9adb42a167036717
Swastik <sudhakarjnv@gmail.com>
2024-09-03 20:37:48 +0530

    pySelenium

--- PythonSelenium/uiControl.py
+++ PythonSelenium/uiControl.py
@@ -1,60 +1,34 @@
 
 from selenium import webdriver
 import time
-from selenium.webdriver.support.select import Select
+
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
-
-driver = webdriver.Edge()
+driver = webdriver.Firefox()
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 
-# ###checkboxes Examples done
-# driver.find_element(By.XPATH,"//input[@id='checkBoxOption1']").click()
+# driver.find_element(By.XPATH, "(//input[@class='radioButton'])[2]").click()
 # driver.refresh()
-# driver.find_element(By.CSS_SELECTOR,"#checkBoxOption2").click()
-# #driver.find_element(By.CLASS_NAME,"//input[@name='checkBoxOption3']")
-# driver.refresh()
-# driver.find_element(By.NAME,"checkBoxOption3").click()
 #
-# #RadioButton Examples
-# driver.find_element(By.XPATH,"(//input[@name='radioButton'])[2]").click()
-# print("Check radio button2")
+# # radioBtn = driver.find_element(By.XPATH, "//input[@class='radioButton']")
+# driver.find_element(By.XPATH,"(//input[@class='radioButton'])[3]").click()
 # driver.refresh()
-# driver.find_element(By.XPATH,"(//input[@type='radio'])[3]").click()
 
-# static Dropdown
+staticDropdown = Select(driver.find_element(By.XPATH, "//select[@id='dropdown-class-example']"))
+staticDropdown.select_by_visible_text("Option2")
 
-#driver.find_element(By.XPATH,"//select[@name='dropdown-class-example']").click()
-# dropdown = Select(driver.find_element(By.XPATH,"//select[@name='dropdown-class-example']"))
-dropdown = Select(driver.find_element(By.ID,"dropdown-class-example"))
+driver.find_element(By.XPATH, "//input[@id='autocomplete']").send_keys("ind")
+time.sleep(3)
+dynamicDropdown = driver.find_elements(By.CSS_SELECTOR, "li[class='ui-menu-item']")
+# print(len(dynamicDropdown))
 
-#assert (dropdown.select_by_index(3)=="Option3")
-#select_by_index() does not return anything, so showing error. It is comparing None with value
-
-dropdown.select_by_index(3)
-
-# autosuggestion examples
-driver.find_element(By.ID,"autocomplete").send_keys("ind")
+for country in dynamicDropdown:
+    if country.text == "India":
+        country.click()
+        break
 time.sleep(1)
-suggestion_list = driver.find_elements(By.ID,"autocomplete")
-# print(len(suggestion_list))
-# for choice in suggestion_list:
-#     if choice.text == "Ind":
-#         choice.click()
-#         break
+
+assert (driver.find_element(By.XPATH, "//input[@id='autocomplete']").get_attribute("value")) == "India"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-time.sleep(2)
