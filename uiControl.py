817361745749580c20ac32b533f43de36fbc1f2c
Sudhakar Shreya <sudhakarjnv@gmail.com>
2024-09-03 10:57:22 +0530

    sud0903

--- uiControl.py
+++ uiControl.py
@@ -3,6 +3,10 @@ import time
 
 driver = webdriver.Firefox()
 driver.get("http://google.com")
+driver = webdriver.Firefox()
+driver.get("http://google.com")
+driver = webdriver.Firefox()
+driver.get("http://google.com")
 
 
 time.sleep(2)
